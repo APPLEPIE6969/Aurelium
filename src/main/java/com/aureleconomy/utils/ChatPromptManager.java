8863bf23c5209f54012407372129d097475195bf
NanoBotAgent <nanobotaiagent@gmail.com>
2026-05-07 18:54:59 +0200

    refactor: migrate ChatPromptManager from deprecated AsyncPlayerChatEvent to Paper AsyncChatEvent

--- src/main/java/com/aureleconomy/utils/ChatPromptManager.java
+++ src/main/java/com/aureleconomy/utils/ChatPromptManager.java
@@ -1,12 +1,13 @@
 package com.aureleconomy.utils;
 
 import com.aureleconomy.AurelEconomy;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.HashMap;
@@ -28,13 +29,12 @@ public class ChatPromptManager implements Listener {
         pendingPrompts.put(player.getUniqueId(), onChat);
     }
 
-    @SuppressWarnings("deprecation")
     @EventHandler(priority = EventPriority.LOWEST)
-    public void onChat(AsyncPlayerChatEvent event) {
+    public void onChat(AsyncChatEvent event) {
         Player player = event.getPlayer();
         if (pendingPrompts.containsKey(player.getUniqueId())) {
             event.setCancelled(true);
-            String message = event.getMessage();
+            String message = PlainTextComponentSerializer.plainText().serialize(event.message());
             Consumer<String> action = pendingPrompts.remove(player.getUniqueId());
 
             // Run action synchronously
