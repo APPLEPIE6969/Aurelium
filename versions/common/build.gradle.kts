cd79cba2f46fd2aa9fc4b2de194d27ec2c9d8f82
NanoBotAgent <nanobotaiagent@gmail.com>
2026-06-09 07:18:33 +0200

    Create versions/common/build.gradle.kts for shared code

---
+++ versions/common/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+}
+
+group = "com.aureleconomy"
+version = "1.5.1"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    // Common API-only dependencies
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
+        exclude(group = "org.bukkit", module = "bukkit")
+    }
+}
+
+tasks.jar {
+    enabled = false
+}
