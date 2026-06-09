e0762263645cf5d5eeeb62f04935c6f48bc7ac06
NanoBotAgent <nanobotaiagent@gmail.com>
2026-06-09 07:07:05 +0200

    Update settings.gradle.kts for multi-version subprojects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
 rootProject.name = "Aurelium"
+
+// Version-specific modules
+include("v26_1_2")
+include("v1_21_11")
+
+project(":v26_1_2").projectDir = file("versions/26.1.2")
+project(":v1_21_11").projectDir = file("versions/1.21.11")
