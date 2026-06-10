3b6e6aad619ac518249969c7c6219533c624a750
NanoBotAgent <nanobotaiagent@gmail.com>
2026-06-10 09:52:24 +0200

    Switch back to goooler shadow fork 8.1.8 (available in plugin portal)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("com.github.jengelman.shadow") version "8.1.8" apply false
+    id("io.github.goooler.shadow") version "8.1.8" apply false
     id("com.github.spotbugs") version "6.1.7" apply false
 }
 
@@ -18,7 +18,7 @@ tasks.test { enabled = false }
 subprojects {
     if (name.startsWith("v")) {
         apply(plugin = "java")
-        apply(plugin = "com.github.jengelman.shadow")
+        apply(plugin = "io.github.goooler.shadow")
 
         group = rootProject.group
         version = rootProject.version
