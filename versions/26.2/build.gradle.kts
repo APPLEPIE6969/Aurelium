5034abe8f2f7bad3ab357bedae514e9cea37ba62
NanoBotAgent <nanobotaiagent@gmail.com>
2026-06-18 00:21:40 +0200

    Fix 26.2 version: use 26.2.build.21-alpha (correct Maven artifact format)

--- versions/26.2/build.gradle.kts
+++ versions/26.2/build.gradle.kts
@@ -17,8 +17,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:26.2-rc-2")
-    implementation("io.papermc.paper:paper-server:26.2-rc-2")
+    compileOnly("io.papermc.paper:paper-api:26.2.build.21-alpha")
 
     implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("com.mysql:mysql-connector-j:8.3.0")
@@ -27,7 +26,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     testImplementation("org.mockito:mockito-core:5.23.0")
     testImplementation("org.mockito:mockito-junit-jupiter:5.23.0")
-    testImplementation("io.papermc.paper:paper-api:26.2-rc-2")
+    testImplementation("io.papermc.paper:paper-api:26.2.build.21-alpha")
     testImplementation("net.kyori:adventure-api:4.17.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testRuntimeOnly("org.xerial:sqlite-jdbc:3.45.3.0")
