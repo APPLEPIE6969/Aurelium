8bbdae12db40e82c4ca44322f54150c70f1739e1
NanoBotAgent <nanobotaiagent@gmail.com>
2026-06-20 12:24:09 +0200

    Add v26_2 module to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,9 +4,11 @@ rootProject.name = "Aurelium"
 include("common")
 
 // Version-specific modules
+include("v26_2")
 include("v26_1_2")
 include("v1_21_11")
 
 project(":common").projectDir = file("versions/common")
+project(":v26_2").projectDir = file("versions/26.2")
 project(":v26_1_2").projectDir = file("versions/26.1.2")
 project(":v1_21_11").projectDir = file("versions/1.21.11")
