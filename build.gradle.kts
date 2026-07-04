9923b749dadb06fb379349a8bdf6ad30b3d303ee
NanoBotAgent <nanobotaiagent@gmail.com>
2026-07-04 14:01:37 +0200

    Bump version to 1.5.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.aureleconomy"
-version = "1.5.3"
+version = "1.5.4"
 
 // Root project doesn't produce a JAR — all builds happen in version subprojects
 tasks.compileJava { enabled = false }
